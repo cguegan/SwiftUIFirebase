3a9626ca0d2c6eb832f8a20c2cd5baca663c6aa4
Christophe Guegan <cguegan@gmail.com>
2025-08-10 07:05:16 +0200

    Initial Commit

---
+++ SwiftUIFirebase/SwiftUIFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseApp.swift
+//  SwiftUIFirebase
+//
+//  Created by Christophe Guégan on 10/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
